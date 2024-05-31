2389fa4637ff3fb30922b5a2555b1b7f042969fe
kady <161952508+kadyzahara@users.noreply.github.com>
2024-05-31 18:27:21 -0400

    Feat(doc): Objetivos de la investigación Adjuntados

---
+++ Objetivos.py
@@ -0,0 +1,21 @@
+
+#Objetivo General 
+-Analizar las problemáticas y los estigmas de las enfermedades mentales dentro de la industria tecnológica en el año 2016.
+
+#Objetivos Específicos
+-Determinar el número de personas diagnosticadas con enfermedades mentales en la industria tecnológica en el año 2016 y analizar si existe un historial familiar dentro de este ámbito.
+⁠-Identificar las 5 enfermedades mentales más comunes entre los trabajadores de la industria tecnológica en diferentes regiones, y evaluar si este ranking varía en Estados Unidos al considerar factores como la edad, género y raza.
+-Calcular el porcentaje de contratación de personas con enfermedades mentales por parte de empresas de distintos tamaños (macro, micro y medianas) en la industria tecnológica en el año 2016, y analizar si ofrecen beneficios o convenios laborales especiales.
+-Investigar si existen casos en los que personas sin enfermedades mentales creen padecer alguna, determinar la proporción de este grupo y las enfermedades que creen tener.
+⁠-Identificar la enfermedad mental más común dentro de cada puesto de trabajo en la industria tecnológica en 2016, y analizar cuántos de estos trabajadores son autónomos o trabajan de forma remota.
+-I-nvestigar si las personas sin enfermedades mentales perciben alguna ventaja laboral hacia aquellos que sí las padecen en la industria tecnológica.
+-Analizar si el nivel de productividad de los trabajadores con enfermedades mentales varía según su rango de edad y el tipo de enfermedad diagnosticada en el sector tecnológico en el año 2016.
+
+#Preguntas de Investigación
+-¿Cuántas personas en la industria tecnológica tienen una enfermedad mental diagnosticada y, dentro de este grupo, existe algún historial familiar dentro de este ámbito?
+-¿Cuáles son las 5 enfermedades más comunes de los encuestados dependiendo de su región? ¿En Estados Unidos cambiaría este ranking si se toma en cuenta la edad, el género y raza? 
+-¿Cuál es el porcentaje de contratación que tienen las empresas (macro, micro y media) a la hora de emplear personas con enfermedades mentales? ¿Estas ofrecen algún beneficio o algún tipo de convenio de trabajo?
+-¿Dentro del grupo de personas que no tienen una enfermedad mental existen casos donde el encuestado crea tener alguna? ¿Qué proporción de este grupo representan estos individuos y qué enfermedades mentales creen padecer?
+-¿Cuál es la enfermedad mental más común dentro de cada puesto de trabajo en la industria tecnológica? ¿De este grupo cuántos son su propio jefe o trabajan remotamente?
+-¿Las personas que no tienen estas enfermedades mentales creen que exista algún tipo de ventaja laboral hacia las personas que sí la padecen? 
+-¿El nivel de productividad de los trabajadores con enfermedades mentales varía según su rango de edad y tipo de enfermedad diagnosticada?
