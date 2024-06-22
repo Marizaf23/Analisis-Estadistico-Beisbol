2113ec673f34cb22ad83d389cebc33b9c4648939
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-22 15:39:36 -0400

    feat: Librerías actualizadas.

--- app.py
+++ app.py
@@ -2,10 +2,6 @@
 import pandas as pd
 import numpy  as np
 import sqlite3
-import plotly.express as px
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import seaborn as sn
 import streamlit as st
 
 
