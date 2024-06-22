e873e0c3b55446080d953f306b1349822fe9df14
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-22 14:59:17 -0400

    feat: Actualización de código para Pregunta #1.

--- app.py
+++ app.py
@@ -67,22 +67,6 @@ st.write("""
 # PREGUNTA #1
 #¿Cuántas personas en la industria tecnológica tienen una enfermedad mental diagnosticada y, dentro de este grupo, existe algún historial familiar dentro de este ámbito?
 
-Pregunta1 = pd.crosstab(df_Investigacion['¿Alguna Vez Has Sido Diagnosticado con una Enfermedad Mental?'], df_Investigacion['Historial Familiar'], 
-                              rownames=['Historial Familiar'], 
-                              colnames=['¿Alguna Vez Has Sido Diagnosticado con una Enfermedad Mental?'])
-print(Pregunta1.to_string())                            
-pd.set_option('display.width', -1)
-pd.set_option('display.max_columns', None)
-print(Pregunta1)
-
-# Agregar columna de totales
-Pregunta1 = Pregunta1.assign(Total=Pregunta1.sum(axis=1))
-
-# Agregar fila de totales
-Pregunta1.loc['Total'] = Pregunta1.sum(axis=0)
-
-
-# %%
 Pregunta1 = pd.crosstab(df_Investigacion['¿Alguna Vez Has Sido Diagnosticado con una Enfermedad Mental?'], 
                         df_Investigacion['Historial Familiar'], 
                         margins=True, 
@@ -91,6 +75,62 @@ Pregunta1 = pd.crosstab(df_Investigacion['¿Alguna Vez Has Sido Diagnosticado co
 Pregunta1.index.name = 'Historial Familiar'
 Pregunta1.columns.name = '¿Alguna Vez Has Sido Diagnosticado con una Enfermedad Mental?'
 
-print(Pregunta1)
+# Renderizar la tabla en formato HTML
+html_table = Pregunta1.to_html()
 
-st.table(Pregunta1)
+print(html_table)
+
+
+# %%
+Pregunta1 = pd.crosstab(df_Investigacion['¿Alguna Vez Has Sido Diagnosticado con una Enfermedad Mental?'], 
+                        df_Investigacion['Historial Familiar'], 
+                        margins=True, 
+                        margins_name='Total')
+
+Pregunta1.columns.name = 'Historial Familiar'
+Pregunta1.index.name = '¿Tienes Alguna Enfermedad Mental?'
+
+
+# %%
+
+import streamlit as st
+
+# Call set_page_config() as the first Streamlit command
+st.set_page_config(
+    page_title="My App",
+    page_icon=":shark:",
+    layout="wide"
+)
+
+# Rest of your Streamlit app code goes here
+st.write("Hello, World!")
+
+# Título de la aplicación
+st.title("Problemáticas y Estigmas de las Enfermedades Mentales en la Industria Tecnológica Estadounidense en el Año 2016")
+
+
+# Crea un menú de pestañas
+tab1, tab2 = st.tabs(["Página 1", "Página 2"])
+
+# Contenido de la página 1
+with tab1:
+    st.header("Página 1")
+    st.write("Este es el contenido de la página 1.")
+    
+# Contenido de la página 2
+with tab2:
+    st.header("Página 2")
+    st.write("Este es el contenido de la página 2.")
+    
+
+st.dataframe(df_Investigacion)
+
+st.write("""
+## Introducción
+
+""")
+
+st.write("Tabla")
+st.write(Pregunta1)
+
+#%
