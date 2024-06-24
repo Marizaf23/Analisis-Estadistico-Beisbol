499815ad2ee9d74ebfb63820efba593d3b94f404
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-24 17:23:43 -0400

    feat: Imagen de fondo agregada.

--- pages/1_Introducción.py
+++ pages/1_Introducción.py
@@ -7,6 +7,28 @@ import streamlit as st
 import requests
 from io import StringIO
 
+def set_bg_hack_url():
+    ''' 
+    A function to unpack an image from url and set as bg. 
+    Returns 
+    ------- 
+    The background. 
+    ''' 
+          
+    st.markdown( 
+          f""" 
+          <style> 
+         .stApp {{ 
+              background: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.com%2Fpulse%2Fusing-artificial-intelligence-help-mental-health-shelley-goodman-mba-kpudc&psig=AOvVaw1mQVphZiA_S7XdkzNLhYj2&ust=1719350349209000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCID6sq6V9YYDFQAAAAAdAAAAABAp"); 
+              background-size: cover 
+          }} 
+          </style> 
+          """, 
+          unsafe_allow_html=True 
+      ) 
+
+set_bg_hack_url()
+
 # Título de la aplicación
 st.title("Problemáticas y Estigmas de las Enfermedades Mentales en la Industria Tecnológica Estadounidense 2016-2019")
 
