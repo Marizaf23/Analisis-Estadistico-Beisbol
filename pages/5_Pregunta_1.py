4cca63805dd7a0af658692864a0d24ef75361929
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-24 23:43:24 -0400

    feat: Actualización de librerías.

--- pages/5_Pregunta_1.py
+++ pages/5_Pregunta_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy  as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly as px
+import plotly.graph_objects as go
 import streamlit as st
 import requests
 from io import StringIO
