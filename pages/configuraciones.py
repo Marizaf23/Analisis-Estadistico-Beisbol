6473d216607ada73aa502ca78adf5a4ba578dc13
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-24 21:04:57 -0400

    feat: Imagen de background agregada.

---
+++ pages/configuraciones.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://i.pinimg.com/564x/97/7b/9c/977b9cc57b8638a0aab8a39ea22736dd.jpg");
+    }
+   </style>
+    """,
+    unsafe_allow_html=True
+)
