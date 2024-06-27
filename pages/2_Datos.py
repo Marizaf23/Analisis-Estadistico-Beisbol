ec12184bc7ad28a106b0762079f6cc8a23bb9ff8
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-27 03:45:00 -0400

    feat: Se agregan estadísticas y gráficas de variables cuantitativas.

--- pages/2_Datos.py
+++ pages/2_Datos.py
@@ -49,3 +49,44 @@ elif option == '2018':
     st.dataframe(df_2018)
 elif option == '2019':
     st.dataframe(df_2019)
+
+
+st.header("Variables Cuantitativas")
+st.subheader("Estadísticas Descriptivas de Edad por Año")
+
+Edad_2016 = df_2016.dropna(subset=['Edad'])['Edad'].describe().to_frame().T.round(2)
+
+Edad_2017 = df_2017['Edad'].describe().to_frame().T.round(2)
+
+Edad_2018 = df_2018['Edad'].describe().to_frame().T.round(2)
+
+Edad_2019 = df_2019.dropna(subset=['Edad'])['Edad'].describe().to_frame().T.round(2)
+
+# Unir los DataFrames en uno
+Edad_inv = pd.concat([Edad_2016, Edad_2017, Edad_2018, Edad_2019], ignore_index=True)
+
+# Renombrar las filas con los años correspondientes
+Edad_inv.index = ['2016', '2017', '2018', '2019']
+
+# Renombrar las columnas
+Edad_inv.columns = ['Muestra', 'Media', 'SD', 'Min', 'Q1', 'Md', 'Q3', 'Máx']
+
+Edad_inv = Edad_inv[['Muestra', 'Media', 'Md', 'SD', 'Min', 'Máx', 'Q1', 'Q3']]
+
+st.dataframe(Edad_inv)
+
+Edad_inv_graph = Edad_inv.drop(['Muestra', 'SD'], axis=1)
+
+# Renombrar las columnas
+Edad_inv_graph.columns = ['mean', '50%', 'min', 'max', '25%', '75%']
+
+fig_edad = px.box(Edad_inv_graph, x=Edad_inv_graph.index, y=Edad_inv_graph.columns, 
+                 title='Distribución de Edades por Año', 
+                 color_discrete_sequence=['#0033cc'])
+
+fig_edad.update_layout(
+    xaxis_title="Año",
+    yaxis_title="Edades"
+)
+
+st.plotly(fig_edad, use_container_width=True)
