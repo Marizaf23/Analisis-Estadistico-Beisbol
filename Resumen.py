53d371d71a11666fbd5929090032a958374a0150
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-28 00:13:35 -0400

    Pregunta 3.

--- Resumen.py
+++ Resumen.py
@@ -345,114 +345,30 @@ st.plotly_chart(figp2, use_container_width=True)
 st.header("Pregunta 3")
 st.subheader('Según la cantidad de empleados en las empresas tecnológicas ¿Cuál tiene la mayor cantidad de empleados con enfermedades mentales? ¿Estas ofrecen algún beneficio o algún tipo de convenio?')
 
-option = st.selectbox('Encuesta:', ['1', '2', '3', '4']),
-
-if option == '1':
-    df1 = pd.DataFrame([["1-5", "3", "2"], ["6-25", "19", "9"], ["26-100", "50", "25"], ["100-500", "80", "21"], ["500-1000", "85", "8"], ["Más de 1000","85", "44"]],
+st.subheader("2016")
+df3 = pd.DataFrame ([["1-5", "3", "1"], ["6-25", "24", "3"], ["26-100", "30", "8"], ["100-500", "44", "20"], ["500-1000", "31", "10"], ["Más de 1000","71", "35"]],
                    columns = ["Tamaño de la Empresa", "Cantidad de Personas dentro de la Empresa con una Enfermedad mental diagnosticada", "Cantidad de Empleados con una Enfermedad Mental que saben de los Beneficios que ofrece la empresa para ellos"])
 
-    x = ['1-5', '6-25', '26-100', '100-500', "500-1000","Más de 1000" ]
+random_x = np.random.randint(1, 101, 100)
+random_y = np.random.randint(1, 101, 100)
 
-    figp3_1 = go.Figure(data=[
-        go.Bar(
-            name='Cantidad de Personas con una Enfermedad Mental que conocen los Beneficios que Ofrece la Empresa',
-            x=x,
-            y=[2, 9, 25, 21, 8, 44],
-            marker_color='#b366ff'  # changed color
-            ),
-        go.Bar(
-            name='Cantidad de Personas dentro de la Empresa con una Enfermedad Mental Diagnosticada',
-            x=x,
-            y=[3, 19, 50, 80, 85, 82],
-            marker_color='#cc99ff'  # changed color
-            )
-        ])
+x = ['1-5', '6-25', '26-100', '100-500', "500-1000","Más de 1000" ]
 
-    figp3_1.update_layout(barmode='group', 
-                title= "Cantidad de empleados con una Enfermedad Mental y si Conocen de los Beneficios que Ofrece su Empresa (2016)")
+figp3_3 = go.Figure(data=[
+    go.Bar(
+        name='Cantidad de Personas con una Enfermedad Mental que conocen los Beneficios que Ofrece la Empresa',
+        x=x,
+        y=[1, 3, 8, 20, 10, 35],
+        marker_color='#b366ff'  # changed color
+        ),
+    go.Bar(
+        name='Cantidad de Personas dentro de la Empresa con una Enfermedad Mental Diagnosticada',
+        x=x,
+        y=[3, 24, 30, 44, 31, 71],
+        marker_color='#cc99ff'  # changed color
+        )
+    ])
 
-    st.plotly_chart(figp3_1, use_container_width=True)
+figp3_3.update_layout(barmode='group', title="Cantidad de empleados con Enfermedad Mental y si Conocen de los Beneficios que Ofrece su Empresa (2018)")
 
-elif option == '2':
-    df2 = pd.DataFrame ([["1-5", "7", "0"], ["6-25", "25", "4"], ["26-100", "46", "3"], ["100-500", "107", "37"], ["500-1000", "36", "11"], ["Más de 1000","88", "46"]],
-                   columns = ["Tamaño de la Empresa", "Cantidad de Personas dentro de la Empresa con una Enfermedad mental diagnosticada", "Cantidad de Empleados con una Enfermedad Mental que saben de los Beneficios que ofrece la empresa para ellos"])
-
-    random_x = np.random.randint(1, 101, 100)
-    random_y = np.random.randint(1, 101, 100)
-
-    x = ['1-5', '6-25', '26-100', '100-500', "500-1000","Más de 1000" ]
-
-    figp3_2 = go.Figure(data=[
-        go.Bar(
-            name='Cantidad de Personas con una Enfermedad Mental que conocen los Beneficios que Ofrece la Empresa',
-            x=x,
-            y=[0, 4, 3, 37, 11, 46],
-            marker_color='#b366ff'  # changed color
-            ),
-        go.Bar(
-            name='Cantidad de Personas dentro de la Empresa con una Enfermedad Mental Diagnosticada',
-            x=x,
-            y=[7, 25, 46, 107, 36, 88],
-            marker_color='#cc99ff'  # changed color
-            )
-        ])
-
-    figp3_2.update_layout(barmode='group', title="Cantidad de empleados con Enfermedad Mental y si Conocen de los Beneficios que Ofrece su Empresa (2017)")
-
-    st.plotly_chart(figp3_2, use_container_width=True)
-
-elif option == '3':
-    df3 = pd.DataFrame ([["1-5", "3", "1"], ["6-25", "24", "3"], ["26-100", "30", "8"], ["100-500", "44", "20"], ["500-1000", "31", "10"], ["Más de 1000","71", "35"]],
-                   columns = ["Tamaño de la Empresa", "Cantidad de Personas dentro de la Empresa con una Enfermedad mental diagnosticada", "Cantidad de Empleados con una Enfermedad Mental que saben de los Beneficios que ofrece la empresa para ellos"])
-
-    random_x = np.random.randint(1, 101, 100)
-    random_y = np.random.randint(1, 101, 100)
-
-    x = ['1-5', '6-25', '26-100', '100-500', "500-1000","Más de 1000" ]
-
-    figp3_3 = go.Figure(data=[
-        go.Bar(
-            name='Cantidad de Personas con una Enfermedad Mental que conocen los Beneficios que Ofrece la Empresa',
-            x=x,
-            y=[1, 3, 8, 20, 10, 35],
-            marker_color='#b366ff'  # changed color
-            ),
-        go.Bar(
-            name='Cantidad de Personas dentro de la Empresa con una Enfermedad Mental Diagnosticada',
-            x=x,
-            y=[3, 24, 30, 44, 31, 71],
-            marker_color='#cc99ff'  # changed color
-            )
-        ])
-
-    figp3_3.update_layout(barmode='group', title="Cantidad de empleados con Enfermedad Mental y si Conocen de los Beneficios que Ofrece su Empresa (2018)")
-
-    st.plotly_chart(figp3_3, use_container_width=True)
-
-elif option == '4':
-    df4 = pd.DataFrame ([["1-5", "3", "0"], ["6-25", "15", "3"], ["26-100", "21", "3"], ["100-500", "33", "11"], ["500-1000", "11", "3"], ["Más de 1000","48", "32"]],
-                   columns = ["Tamaño de la Empresa", "Cantidad de Personas dentro de la Empresa con una Enfermedad mental diagnosticada", "Cantidad de Empleados con una Enfermedad Mental que saben de los Beneficios que ofrece la empresa para ellos"])
-
-    random_x = np.random.randint(1, 101, 100)
-    random_y = np.random.randint(1, 101, 100)
-
-    x = ['1-5', '6-25', '26-100', '100-500', "500-1000","Más de 1000" ]
-
-    figp3_4 = go.Figure(data=[
-        go.Bar(
-            name='Cantidad de Personas con una Enfermedad Mental que conocen los Beneficios que Ofrece la Empresa',
-            x=x,
-            y=[0,3, 3, 11, 3, 32],
-            marker_color='#b366ff'
-            ),
-        go.Bar(
-            name='Cantidad de Personas dentro de la Empresa con una Enfermedad Mental Diagnosticada',
-            x=x,
-            y=[3, 15, 21, 33, 11, 48],
-            marker_color='#cc99ff'  # changed color
-            )
-        ])
-
-    figp3_4.update_layout(barmode='group', title="Cantidad de empleados con Enfermedad Mental y si Conocen de los Beneficios que Ofrece su Empresa (2019)")
-
-    st.plotly_chart(figp3_4, use_container_width=True)
+st.plotly_chart(figp3_3, use_container_width=True)
