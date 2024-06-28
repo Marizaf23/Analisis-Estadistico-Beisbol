0f3a6ed12888101e9649f4f5932dc7d2e4a94dd1
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-28 01:22:11 -0400

    feat: Conclusión agregada.

--- Resumen.py
+++ Resumen.py
@@ -535,3 +535,34 @@ preguntatrabajo_5_2016 = pd.crosstab(df_2016[filtro_si_diagnosticado]['¿Cree qu
 preguntatrabajo_5_2016 = preguntatrabajo_5_2016.rename(columns={'Si': ''})
 preguntatrabajo_5_2016 = preguntatrabajo_5_2016.drop('All', axis=0).drop('All', axis=1)  # Elimina las filas y columnas "All"
 st.dataframe(preguntatrabajo_5_2016)
+
+
+st.header("Conclusión")
+st.write("""
+<style>
+p {
+  text-align: justify;
+}
+</style>
+<ul>
+    <li>La enfermedad mental es un problema común en el sector tecnológico estadounidense, que provoca que las personas sufran síntomas como fatiga, falta de concentración y productividad reducida, lo que puede afectar negativamente su desempeño laboral y su bienestar general.
+    <li>Las empresas deben velar por la salud mental de sus trabajadores, dado que si tienen empleados sanos la empresa tendrá mejor rendimiento.
+    <li>Las empresas generalmente no promueven el sistema de apoyo que ofrecen a sus trabajadores, esto podría ocasionar recortes del presupuesto destinados a programas de salud mental, ocasionando que personas que sí reciban el apoyo puedan perderlo eventualmente.
+    <li>El número de personas con enfermedades mentales se mantiene estable a través de los años, esto es un problema ya que de alguna manera no se genera ningún cambio significativo, lo que quiere decir que la situación no mejora.
+</ul>
+<p>En última instancia, todos somos responsables de crear una cultura laboral que valore la salud mental y promueva el bienestar. Juntos, podemos marcar la diferencia y construir un futuro tecnológico más humano y sostenible.</p>
+""", unsafe_allow_html=True)
+
+st.subheader("Recomendaciones")
+st.write("""
+<style>
+p {
+  text-align: justify;
+}
+</style>
+<ul>
+    <li>Promover la Comunicación Abierta: Establecer un espacio seguro para que los empleados compartan sus preocupaciones y desafíos es fundamental. La comunicación abierta reduce el estrés y fomenta un sentido de comunidad.</p>
+    <li>Fomentar el Equilibrio entre el Trabajo y la Vida Personal: Establecer límites claros entre el tiempo de trabajo y el tiempo personal ayuda a los empleados a desconectar y recargar energías, lo que es vital para su bienestar.</p>
+<ul>
+""", unsafe_allow_html=True)
+
