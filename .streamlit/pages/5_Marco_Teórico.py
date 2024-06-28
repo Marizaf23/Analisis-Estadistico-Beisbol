3c487dbda80ea86baafd44a85509fe5a70f9529b
María Ruiz <163086921+Marizaf23@users.noreply.github.com>
2024-06-28 05:57:05 -0400

    feat: App.

--- .streamlit/pages/5_Marco_Teórico.py
+++ .streamlit/pages/5_Marco_Teórico.py
@@ -6,6 +6,20 @@ import plotly as px
 import streamlit as st
 import requests
 from io import StringIO
+import base64
+
+LOGO_IMAGE1 = "logos/UCV.png"
+LOGO_IMAGE2 = "logos/EECA.png"
+
+st.markdown(
+    f"""
+    <div style="background-color:#80bfff;padding:10px;display:flex;justify-content:space-between;align-items:center;margin-top:-30px;">
+        <img src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE1, "rb").read()).decode()}" style="height:40px;margin:30px;">
+        <img src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE2, "rb").read()).decode()}" style="height:40px;margin:30px;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # Título de la aplicación
